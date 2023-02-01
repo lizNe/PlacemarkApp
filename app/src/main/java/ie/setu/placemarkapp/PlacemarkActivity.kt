aca6fc560fcdb434f6e37478dad07352e1b3f61e
lizzz <20071724@mail.wit.ie>
2023-02-01 18:19:50 +0000

    Everything up tp Week 5 working

--- app/src/main/java/ie/setu/placemarkapp/PlacemarkActivity.kt
+++ app/src/main/java/ie/setu/placemarkapp/PlacemarkActivity.kt
@@ -2,10 +2,22 @@ package ie.setu.placemarkapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+import timber.log.Timber.i
 
 class PlacemarkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placemark)
+
+        Timber.plant(Timber.DebugTree())
+       // Timber.i("Placemark Activity started..")
+
+        i("Placemark Activity started...")
+
+
+
+
+
     }
 }
